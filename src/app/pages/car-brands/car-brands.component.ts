4ab4f1c82094a0207bf297a0b0bf54fb14fdf836
krutikagsfj <49934320+krutikagsfj@users.noreply.github.com>
2019-04-24 13:10:24 +0530

    Add files via upload

---
+++ src/app/pages/car-brands/car-brands.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { MySerService } from 'src/app/Service/my-ser.service';
+@Component({
+  selector: 'app-car-brands',
+  templateUrl: './car-brands.component.html',
+  styleUrls: ['./car-brands.component.css']
+})
+
+
+export class CarBrandsComponent implements OnInit {
+  
+
+  
+
+  resData;
+  constructor(private pser:MySerService){}
+  ngOnInit() 
+  {
+    this.pser.getProducts()
+    .subscribe(res=>
+      {
+        this.resData=res;
+        console.log(res);
+      },err=>
+      {
+        console.log(err);
+      })
+  }
+
+}
